58a3cf0975b93b103ce2941ec1c4b8745534e3c7
Karina G <kgonzaleztech@gmail.com>
2021-08-14 16:07:26 -0700

    resume-report-ui WIP sample barchart

---
+++ force-app/main/default/lwc/parentbarchart/parentbarchart.js
@@ -0,0 +1,39 @@
+import { LightningElement, wire } from 'lwc';
+import getOpportunities from '@salesforce/apex/BarChartController.getOpportunities'; //@salesforce/apex/Namespace.Classname.apexMethodReference
+export default class parentbarchart extends LightningElement {
+    chartConfiguration;
+    @wire(getOpportunities)
+    getOpportunities({ error, data }) {
+        if (error) {
+            this.error = error;
+            this.chartConfiguration = undefined;
+        } else if (data) {
+            let chartAmtData = [];
+            let chartRevData = [];
+            let chartLabel = [];
+            data.forEach(opp => {
+                chartAmtData.push(opp.amount);
+                chartRevData.push(opp.expectRevenue);
+                chartLabel.push(opp.stage);
+            });
+            this.chartConfiguration = {
+                type: 'bar',
+                data: {
+                    datasets: [{
+                        label: 'Amount',
+                        backgroundColor: "green",
+                        data: chartAmtData,
+                    },{
+                        label: 'Expected Revenue',
+                        backgroundColor: "orange",
+                        data: chartRevData,
+                    },],
+                    labels: chartLabel,
+                },
+                options: {},
+            };
+            console.log('data => ', data);
+            this.error = undefined;
+        }
+    }
+}
