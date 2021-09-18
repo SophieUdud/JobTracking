d75c0fc322bb06d792d651b3954ddfd8361bd550
Karina G <siriuscoder108@yahoo.com>
2021-09-18 16:27:07 -0700

    Converted from bar chart to stacked bar chart

--- force-app/main/default/lwc/parentbarchart/parentbarchart.js
+++ force-app/main/default/lwc/parentbarchart/parentbarchart.js
@@ -18,19 +18,69 @@ export default class parentbarchart extends LightningElement {
             });
             this.chartConfiguration = {
                 type: 'bar',
+                // data: {
+                //     datasets: [{
+                //         label: 'Stage1',
+                //         backgroundColor: "green",
+                //         data: [5, 3, 2],
+                //         //data: chartAmtData,
+                //     },{
+                //         label: 'Stage2',
+                //         backgroundColor: "orange",
+                //         data: [1, 2, 3],
+                //         //data: chartRevData,
+                //     },],
+                //     labels: chartLabel,
+                // },
+                // options: {
+               
+                // },
                 data: {
+                    labels: ["<  1","1 - 2","3 - 4","5 - 9","10 - 14","15 - 19","20 - 24","25 - 29","> - 29"],
                     datasets: [{
-                        label: 'Amount',
-                        backgroundColor: "green",
-                        data: chartAmtData,
-                    },{
-                        label: 'Expected Revenue',
-                        backgroundColor: "orange",
-                        data: chartRevData,
-                    },],
-                    labels: chartLabel,
-                },
-                options: {},
+                      label: 'Employee',
+                      backgroundColor: "#caf270",
+                      data: [12, 59, 5, 56, 58,12, 59, 87, 45],
+                    }, {
+                      label: 'Engineer',
+                      backgroundColor: "#45c490",
+                      data: [12, 59, 5, 56, 58,12, 59, 85, 23],
+                    }, {
+                      label: 'Government',
+                      backgroundColor: "#008d93",
+                      data: [12, 59, 5, 56, 58,12, 59, 65, 51],
+                    }, {
+                      label: 'Political parties',
+                      backgroundColor: "#2e5468",
+                      data: [12, 59, 5, 56, 58, 12, 59, 12, 74],
+                    }],
+                  },                
+                options: {
+                    tooltips: {
+                      displayColors: true,
+                      callbacks:{
+                        mode: 'x',
+                      },
+                    },
+                    scales: {
+                      xAxes: [{
+                        stacked: true,
+                        gridLines: {
+                          display: false,
+                        }
+                      }],
+                      yAxes: [{
+                        stacked: true,
+                        ticks: {
+                          beginAtZero: true,
+                        },
+                        type: 'linear',
+                      }]
+                    },
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    legend: { position: 'bottom' },
+                  }                
             };
             console.log('data => ', data);
             this.error = undefined;
